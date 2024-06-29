e07da342d7cb0f37a10dc78a2699744ab3989587
Allysson Alas <allyssonmgs@gmail.com>
2024-06-29 14:13:22 -0300

    chore: add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import { fixupConfigRules } from '@eslint/compat'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  { ignores: ['node_modules', 'dist', 'public'] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  eslintConfigPrettier
+]
