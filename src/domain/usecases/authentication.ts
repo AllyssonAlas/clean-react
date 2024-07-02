dd4131c818ae9007eb0f40e8e36aa6266fcf8150
Allysson Alas <allyssonmgs@gmail.com>
2024-07-02 20:56:35 -0300

    feat: ensure Authentication returns an accessToken on success

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,16 +1,18 @@
+import { AccountModel } from '@/domain/models'
 import { HttpPostClient, HttpStatusCode } from '@/domain/contracts/gateways'
-import { InvalidCredentialsError, UnexpectedError } from '../errors'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 
 type Input = { email: string; password: string }
-export type Authentication = (input: Input) => Promise<void>
-type Setup = (url: string, httpPostClient: HttpPostClient<Input>) => Authentication
+type Output = AccountModel
+export type Authentication = (input: Input) => Promise<Output>
+type Setup = (url: string, httpPostClient: HttpPostClient<Input, Output>) => Authentication
 
 export const setupAuthentication: Setup = (url, httpPostClient) => {
   return async (input) => {
     const response = await httpPostClient.post({ url, body: input })
     switch (response.statusCode) {
       case HttpStatusCode.ok:
-        break
+        return response.body
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError()
       default:
