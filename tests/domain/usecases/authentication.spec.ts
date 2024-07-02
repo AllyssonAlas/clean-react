2f25cdf16a1e4b84fca7cf0b7d0c5186472be326
Allysson Alas <allyssonmgs@gmail.com>
2024-07-02 20:27:15 -0300

    test: ensure Authentication throws UnexpectedError on default cases

--- tests/domain/usecases/authentication.spec.ts
+++ tests/domain/usecases/authentication.spec.ts
@@ -2,7 +2,7 @@ import { mock, MockProxy } from 'jest-mock-extended'
 
 import { Authentication, setupAuthentication } from '@/domain/usecases'
 import { HttpPostClient, HttpStatusCode } from '@/domain/contracts/gateways'
-import { InvalidCredentialsError } from '@/domain/errors'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 
 import { mockAuthenticationInput } from '@/tests/domain/mocks'
 
@@ -15,7 +15,7 @@ describe('Authentication', () => {
     url = 'any_url'
     httpPostClient = mock()
     httpPostClient.post.mockResolvedValue({
-      statusCode: HttpStatusCode.noContent,
+      statusCode: HttpStatusCode.ok,
     })
   })
 
@@ -37,5 +37,29 @@ describe('Authentication', () => {
     })
     const promise = sut(mockAuthenticationInput())
     await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+  })
+
+  it('Should throw if HttpPostClient returns 400', async () => {
+    httpPostClient.post.mockResolvedValueOnce({
+      statusCode: HttpStatusCode.badRequest,
+    })
+    const promise = sut(mockAuthenticationInput())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('Should throw if HttpPostClient returns 404', async () => {
+    httpPostClient.post.mockResolvedValueOnce({
+      statusCode: HttpStatusCode.notFound,
+    })
+    const promise = sut(mockAuthenticationInput())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('Should throw if HttpPostClient returns 500', async () => {
+    httpPostClient.post.mockResolvedValueOnce({
+      statusCode: HttpStatusCode.serverError,
+    })
+    const promise = sut(mockAuthenticationInput())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
