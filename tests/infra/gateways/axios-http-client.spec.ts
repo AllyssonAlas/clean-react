b6596b3939f41f266d38b19be864ef2d52ea1fc2
Allysson Alas <allyssonmgs@gmail.com>
2024-07-06 14:30:09 -0300

    refactor: isolate sut creation

--- tests/infra/gateways/axios-http-client.spec.ts
+++ tests/infra/gateways/axios-http-client.spec.ts
@@ -5,11 +5,24 @@ import { AxiosHttpClient } from '@/infra/gateways'
 jest.mock('axios')
 
 describe('AxiosHttpClient', () => {
-  it('Should call post with correct input', async () => {
-    const fakeAxios = axios as jest.Mocked<typeof axios>
-    const sut = new AxiosHttpClient()
+  let input: {
+    url: string
+    body: object
+  }
+  let fakeAxios: jest.Mocked<typeof axios>
+  let sut: AxiosHttpClient
 
-    await sut.post({ url: 'any_url', body: { any: 'any' } })
+  beforeAll(() => {
+    input = { url: 'any_url', body: { any: 'any' } }
+    fakeAxios = axios as jest.Mocked<typeof axios>
+  })
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient()
+  })
+
+  it('Should call post with correct input', async () => {
+    await sut.post(input)
 
     expect(fakeAxios.post).toHaveBeenCalledWith('any_url', { any: 'any' })
     expect(fakeAxios.post).toHaveBeenCalledTimes(1)
