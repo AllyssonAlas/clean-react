46ef3915b05a9a110d58f2f577eabc6f8f228f12
Allysson Alas <allyssonmgs@gmail.com>
2024-07-06 15:19:05 -0300

    feat: ensure AxiosHttpClient returns correct output error from axios.post

--- src/infra/gateways/axios-http-client.ts
+++ src/infra/gateways/axios-http-client.ts
@@ -4,7 +4,16 @@ import { HttpPostClient } from '@/domain/contracts/gateways'
 
 export class AxiosHttpClient {
   async post({ url, params }: HttpPostClient.Input): Promise<HttpPostClient.Output> {
-    const { data, status } = await axios.post(url, params)
-    return { body: data, statusCode: status }
+    try {
+      const { data, status } = await axios.post(url, params)
+      return { body: data, statusCode: status }
+    } catch (error) {
+      if (error.response) {
+        return {
+          body: error.response.data,
+          statusCode: error.response.status,
+        }
+      }
+    }
   }
 }
