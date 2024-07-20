2e90da5cb982ea82885850789bac504bf14e8c5e
Allysson Alas <allyssonmgs@gmail.com>
2024-07-20 17:08:50 -0300

    test: ensure Login will not render spinner and error on start

---
+++ tests/presentation/pages/login.spec.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react'
+
+import { Login } from '@/presentation/pages'
+
+describe('Login Page', () => {
+  it('Should not render spinner and error on start', () => {
+    const { getByTestId } = render(<Login />)
+
+    const errorWrap = getByTestId('error-wrap')
+
+    expect(errorWrap.childElementCount).toBe(0)
+  })
+})
