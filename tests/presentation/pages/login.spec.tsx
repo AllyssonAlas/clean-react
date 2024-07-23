17a3d269deb395cf9b334070ffb69d596f80a7be
Allysson Alas <allyssonmgs@gmail.com>
2024-07-23 19:25:34 -0300

    refactor: isolate sut creation

--- tests/presentation/pages/login.spec.tsx
+++ tests/presentation/pages/login.spec.tsx
@@ -1,15 +1,19 @@
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 
 import { Login } from '@/presentation/pages'
 
 describe('Login Page', () => {
-  it('Should start with initial state', () => {
-    const { getByTestId } = render(<Login />)
+  let sut: RenderResult
 
-    const errorWrap = getByTestId('error-wrap')
-    const submitButton = getByTestId('submit') as HTMLButtonElement
-    const emailStatus = getByTestId('email-status')
-    const passwordStatus = getByTestId('password-status')
+  beforeEach(() => {
+    sut = render(<Login />)
+  })
+
+  it('Should start with initial state', () => {
+    const errorWrap = sut.getByTestId('error-wrap')
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+    const emailStatus = sut.getByTestId('email-status')
+    const passwordStatus = sut.getByTestId('password-status')
 
     expect(errorWrap.childElementCount).toBe(0)
     expect(submitButton.disabled).toBe(true)
