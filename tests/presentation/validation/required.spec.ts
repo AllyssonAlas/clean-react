bb9503b322e55e1f39a99c575e4dcaf33d725327
Allysson Alas <allyssonmgs@gmail.com>
2024-08-10 19:56:47 -0300

    test: ensure Required returns RequiredFieldError message if input does not contain field

---
+++ tests/presentation/validation/required.spec.ts
@@ -0,0 +1,12 @@
+import { Required } from '@/presentation/validation'
+import { RequiredFieldError } from '@/presentation/errors'
+
+describe('Required', () => {
+  it('Should return error if input does not contain field', () => {
+    const sut = new Required('field')
+
+    const error = sut.validate({})
+
+    expect(error).toEqual(new RequiredFieldError().message)
+  })
+})
