524a26987354bd15f910ddd42bff42bcced5afb0
Allysson Alas <allyssonmgs@gmail.com>
2024-08-14 23:34:55 -0300

    refactor: isolate sut creation

--- tests/presentation/validation/composite.spec.ts
+++ tests/presentation/validation/composite.spec.ts
@@ -1,18 +1,30 @@
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 import { ValidationComposite } from '@/presentation/validation'
 import { FieldValidation } from '@/presentation/protocols'
 
 describe('ValidationComposite', () => {
+  let validators: MockProxy<FieldValidation>[]
+  let sut: ValidationComposite
+
+  beforeEach(() => {
+    const fieldValidation1 = mock<FieldValidation>()
+    const fieldValidation2 = mock<FieldValidation>()
+    validators = [fieldValidation1, fieldValidation2].map((mock) => {
+      mock.field = 'any_field'
+      mock.validate.mockReturnValue(undefined)
+      return mock
+    })
+    sut = new ValidationComposite(validators)
+  })
+
   it('Should return error if any validation fails', () => {
-    const fieldValidationSpy = mock<FieldValidation>()
-    fieldValidationSpy.field = 'any_field'
-    fieldValidationSpy.validate.mockReturnValue(new Error('first_error').message)
-    const fieldValidationSpy2 = mock<FieldValidation>()
-    fieldValidationSpy2.field = 'any_field'
-    fieldValidationSpy2.validate.mockReturnValue(new Error('second_error').message)
-    const sut = new ValidationComposite([fieldValidationSpy, fieldValidationSpy2])
+    validators.forEach((validator, index) => {
+      validator.validate.mockReturnValueOnce(new Error(`message_error_${index}`).message)
+    })
+
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('first_error')
+
+    expect(error).toBe('message_error_0')
   })
 })
