6a4dac3d29e999127bfdd166fccf8e6dd7e2d202
Allysson Alas <allyssonmgs@gmail.com>
2024-08-19 19:25:49 -0300

    test: ensure ValidationBuilder adds RequiredEmail to validators array

--- tests/presentation/validation/builder.spec.ts
+++ tests/presentation/validation/builder.spec.ts
@@ -1,9 +1,15 @@
-import { ValidationBuilder as Builder, Required } from '@/presentation/validation'
+import { ValidationBuilder as Builder, Required, RequiredEmail } from '@/presentation/validation'
 
 describe('ValidationBuilder', () => {
   it('Should add Required to validators array', () => {
     const sut = Builder.field('anyField').required().build()
 
     expect(sut).toEqual([new Required('anyField')])
+  })
+
+  it('Should add RequiredEmail to validators array', () => {
+    const sut = Builder.field('anyField').email().build()
+
+    expect(sut).toEqual([new RequiredEmail('anyField')])
   })
 })
