c03b7d0139b9f7dc919f297d2ef78a1bf01a6d53
Allysson Alas <allyssonmgs@gmail.com>
2024-08-19 19:29:49 -0300

    test: ensure ValidationBuilder adds RequiredMinLength to validators array

--- tests/presentation/validation/builder.spec.ts
+++ tests/presentation/validation/builder.spec.ts
@@ -1,4 +1,4 @@
-import { ValidationBuilder as Builder, Required, RequiredEmail } from '@/presentation/validation'
+import { ValidationBuilder as Builder, Required, RequiredEmail, RequiredMinLength } from '@/presentation/validation'
 
 describe('ValidationBuilder', () => {
   it('Should add Required to validators array', () => {
@@ -11,5 +11,11 @@ describe('ValidationBuilder', () => {
     const sut = Builder.field('anyField').email().build()
 
     expect(sut).toEqual([new RequiredEmail('anyField')])
+  })
+
+  it('Should add RequiredMinLength to validators array', () => {
+    const sut = Builder.field('anyField').min(5).build()
+
+    expect(sut).toEqual([new RequiredMinLength('anyField', 5)])
   })
 })
