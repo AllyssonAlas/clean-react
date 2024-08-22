60114f8273a33edd31044a2e8c715f5ce173835d
Allysson Alas <allyssonmgs@gmail.com>
2024-08-22 20:28:04 -0300

    feat: ensure LocalStorageAdapter calls localStorage setItem with correct input

---
+++ src/infra/gateways/local-storage-adapter.ts
@@ -0,0 +1,7 @@
+import { SetStorage } from '@/domain/contracts/gateways'
+
+export class LocalStorageAdapter implements SetStorage {
+  async set({ key, value }: SetStorage.Input): Promise<void> {
+    localStorage.setItem(key, value)
+  }
+}
