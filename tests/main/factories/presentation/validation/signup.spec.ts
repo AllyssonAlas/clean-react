c9bdb5aa7bc14d6b4a6ea1cd21a2237f3509cde2
Allysson Alas <allyssonmgs@gmail.com>
2024-08-27 23:27:00 -0300

    test: ensure Signup ValidationComposite uses correct validations

---
+++ tests/main/factories/presentation/validation/signup.spec.ts
@@ -0,0 +1,27 @@
+import {
+  ValidationComposite,
+  Required,
+  RequiredEmail,
+  RequiredMinLength,
+  RequiredComparison,
+} from '@/presentation/validation'
+import { makeSignupValidation } from '@/main/factories/presentation/validation'
+
+describe('SignUpValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignupValidation()
+
+    expect(composite).toEqual(
+      new ValidationComposite([
+        new Required('name'),
+        new RequiredMinLength('name', 5),
+        new Required('email'),
+        new RequiredEmail('email'),
+        new Required('password'),
+        new RequiredMinLength('password', 5),
+        new Required('passwordConfirmation'),
+        new RequiredComparison('passwordConfirmation', 'password'),
+      ]),
+    )
+  })
+})
