c38bb470e8aeebc47e7060043264dc620d0d4fe4
Allysson Alas <allyssonmgs@gmail.com>
2024-08-31 15:55:23 -0300

    test: ensure Login presents error state if form is invalid

--- tests/cypress/e2e/pages/login.cy.ts
+++ tests/cypress/e2e/pages/login.cy.ts
@@ -9,4 +9,15 @@ describe('Login', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').type('invalid_email')
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo requer um email').should('contain.text', '🔴')
+    cy.getByTestId('password').type('1234')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo requer pelo menos 5 caracteres')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
 })
