3e8b0fd9f5a663c960be1bf4c8fd24a41a0bd89e
Allysson Alas <allyssonmgs@gmail.com>
2024-09-03 13:48:24 -0300

    test: ensure Signup presents error state if form is invalid

--- tests/cypress/e2e/pages/signup.cy.ts
+++ tests/cypress/e2e/pages/signup.cy.ts
@@ -11,4 +11,17 @@ describe('Signup', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('name').type('1234')
+    cy.testInputState('name', 'Campo requer pelo menos 5 caracteres')
+    cy.getByTestId('email').type('invalid_email')
+    cy.testInputState('email', 'Campo requer um email')
+    cy.getByTestId('password').type('1234')
+    cy.testInputState('password', 'Campo requer pelo menos 5 caracteres')
+    cy.getByTestId('passwordConfirmation').type('123')
+    cy.testInputState('passwordConfirmation', 'Campo precisa ser igual a password')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
 })
