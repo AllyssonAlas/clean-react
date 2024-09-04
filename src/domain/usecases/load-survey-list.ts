8bff0b8036f59453b0ca0cfd1821ba10071f38a1
Allysson Alas <allyssonmgs@gmail.com>
2024-09-04 20:23:44 -0300

    feat: ensure LoadSurveyList returns correct output on 200

--- src/domain/usecases/load-survey-list.ts
+++ src/domain/usecases/load-survey-list.ts
@@ -2,15 +2,16 @@ import { SurveyModel } from '@/domain/models'
 import { HttpGetClient, HttpStatusCode } from '@/domain/contracts/gateways'
 import { UnexpectedError } from '@/domain/errors'
 
-export type LoadSurveyList = () => Promise<void>
+type Output = Array<SurveyModel>
+export type LoadSurveyList = () => Promise<Output>
 type Setup = (url: string, httpGetClient: HttpGetClient<Array<SurveyModel>>) => LoadSurveyList
 
 export const setupLoadSurveyList: Setup = (url, httpGetClient) => {
   return async () => {
-    const { statusCode } = await httpGetClient.get({ url })
+    const { statusCode, body } = await httpGetClient.get({ url })
     switch (statusCode) {
       case HttpStatusCode.ok:
-        break
+        return body
       default:
         throw new UnexpectedError()
     }
--- tests/domain/mocks/usecases.ts
+++ tests/domain/mocks/usecases.ts
@@ -9,3 +9,13 @@ export const mockAddAccountInput = () => ({
   password: 'any_password',
   passwordConfirmation: 'any_password',
 })
+
+export const mockLoadSurveyListOutput = () => [
+  {
+    id: 'any_id',
+    question: 'any_question',
+    answers: [{ answer: 'any_answer_1' }, { answer: 'any_answer_2' }],
+    date: new Date('09/07/2022'),
+    didAnswer: true,
+  },
+]
