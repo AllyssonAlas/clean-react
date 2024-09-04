9d2fee957659f7ad5270631577bffec5c6097b01
Allysson Alas <allyssonmgs@gmail.com>
2024-09-04 20:08:30 -0300

    test: ensure LoadSurveyList throws UnexpectedError on 403

--- tests/domain/usecases/load-survey-list.spec.ts
+++ tests/domain/usecases/load-survey-list.spec.ts
@@ -1,16 +1,21 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
+import { SurveyModel } from '@/domain/models'
 import { setupLoadSurveyList, LoadSurveyList } from '@/domain/usecases'
-import { HttpGetClient } from '@/domain/contracts/gateways'
+import { HttpGetClient, HttpStatusCode } from '@/domain/contracts/gateways'
+import { UnexpectedError } from '@/domain/errors'
 
 describe('LoadSurveyList', () => {
   let url: string
-  let httpGetClient: MockProxy<HttpGetClient>
+  let httpGetClient: MockProxy<HttpGetClient<SurveyModel[]>>
   let sut: LoadSurveyList
 
   beforeAll(() => {
     url = 'any_url'
     httpGetClient = mock()
+    httpGetClient.get.mockResolvedValue({
+      statusCode: 200,
+    })
   })
 
   beforeEach(() => {
@@ -22,5 +27,15 @@ describe('LoadSurveyList', () => {
 
     expect(httpGetClient.get).toHaveBeenCalledWith({ url })
     expect(httpGetClient.get).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should throw UnexpectedError if HttpPostClient returns 403', async () => {
+    httpGetClient.get.mockResolvedValueOnce({
+      statusCode: HttpStatusCode.forbidden,
+    })
+
+    const promise = sut()
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
