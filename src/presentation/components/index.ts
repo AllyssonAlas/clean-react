426b9a4ca9d81f7182b1e7b1cac26496234ae62e
Allysson Alas <allyssonmgs@gmail.com>
2024-09-05 19:39:02 -0300

    refactor: move Icon to its own file

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,6 +1,7 @@
 export * from './Footer'
 export * from './FormStatus'
 export * from './Header'
+export * from './Icon'
 export * from './Input'
 export * from './LoginHeader'
 export * from './Logo'
