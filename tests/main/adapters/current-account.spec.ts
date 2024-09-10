3fa82faf8668c33197d39826aaf01e3dc18c9425
Allysson Alas <allyssonmgs@gmail.com>
2024-09-10 20:25:10 -0300

    test: ensure CurrentAccountAdapter calls LocalStorageAdapter with correct input

---
+++ tests/main/adapters/current-account.spec.ts
@@ -0,0 +1,15 @@
+import { LocalStorageAdapter } from '@/infra/gateways'
+import { setCurrentAccountAdapter } from '@/main/adapters'
+
+jest.mock('@/infra/gateways/local-storage-adapter')
+
+describe('CurrentAccountAdapter', () => {
+  it('Should call LocalStorageAdapter.set with correct input', () => {
+    const account = { accessToken: 'any_access_token', name: 'any_name' }
+    const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
+
+    setCurrentAccountAdapter(account)
+
+    expect(setSpy).toHaveBeenCalledWith({ key: 'account', value: account })
+  })
+})
