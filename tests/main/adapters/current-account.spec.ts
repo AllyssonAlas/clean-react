98a7148a11040f8f56c8ef727ff40740fd6dbfed
Allysson Alas <allyssonmgs@gmail.com>
2024-09-11 18:59:14 -0300

    test: ensure getCurrentAccountAdapter calls LocalStorageAdapter.get with correct output

--- tests/main/adapters/current-account.spec.ts
+++ tests/main/adapters/current-account.spec.ts
@@ -1,22 +1,35 @@
 import { UnexpectedError } from '@/domain/errors'
 import { LocalStorageAdapter } from '@/infra/gateways'
-import { setCurrentAccountAdapter } from '@/main/adapters'
+import { setCurrentAccountAdapter, getCurrentAccountAdapter } from '@/main/adapters'
 
 jest.mock('@/infra/gateways/local-storage-adapter')
 
 describe('CurrentAccountAdapter', () => {
-  it('Should call LocalStorageAdapter.set with correct input', () => {
-    const account = { accessToken: 'any_access_token', name: 'any_name' }
-    const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
+  describe('setCurrentAccountAdapter', () => {
+    it('Should call LocalStorageAdapter.set with correct input', () => {
+      const account = { accessToken: 'any_access_token', name: 'any_name' }
+      const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
 
-    setCurrentAccountAdapter(account)
+      setCurrentAccountAdapter(account)
 
-    expect(setSpy).toHaveBeenCalledWith({ key: 'account', value: account })
+      expect(setSpy).toHaveBeenCalledWith({ key: 'account', value: account })
+    })
+
+    it('Should throw UnexpectedError', () => {
+      expect(() => {
+        setCurrentAccountAdapter(undefined)
+      }).toThrow(new UnexpectedError())
+    })
   })
 
-  it('Should throw UnexpectedError', () => {
-    expect(() => {
-      setCurrentAccountAdapter(undefined)
-    }).toThrow(new UnexpectedError())
+  describe('getCurrentAccountAdapter', () => {
+    it('Should call LocalStorageAdapter.get with correct input', () => {
+      const getSpy = jest.spyOn(LocalStorageAdapter.prototype, 'get')
+
+      getCurrentAccountAdapter()
+
+      expect(getSpy).toHaveBeenCalledWith({ key: 'account' })
+      expect(getSpy).toHaveBeenCalledTimes(1)
+    })
   })
 })
