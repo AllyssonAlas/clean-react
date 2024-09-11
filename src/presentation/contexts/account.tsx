7a1b14df395e05609a88aa2aab2f585cfec95fa2
Allysson Alas <allyssonmgs@gmail.com>
2024-09-11 18:28:41 -0300

    feat: add getCurrentAccount to AccountContext

--- src/presentation/contexts/account.tsx
+++ src/presentation/contexts/account.tsx
@@ -4,6 +4,7 @@ import { AccountModel } from '@/domain/models'
 
 type Data = {
   setCurrentAccount?: (account: AccountModel) => void
+  getCurrentAccount?: () => AccountModel
 }
 
 export const AccountContext = createContext<Data>(null)
