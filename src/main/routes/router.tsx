1430ba94e13dcf1d3925008d737e197a17c8ecb4
Allysson Alas <allyssonmgs@gmail.com>
2024-09-12 12:48:40 -0300

    feat: add PrivateRoute

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AccountContext } from '@/presentation/contexts'
 import { MakeLogin, MakeSignup } from '@/main/factories/presentation/pages'
 import { SurveyList } from '@/presentation/pages'
+import { PrivateRoute } from '@/presentation/components'
 import { setCurrentAccountAdapter, getCurrentAccountAdapter } from '@/main/adapters'
 
 import '@/presentation/styles/globals.scss'
@@ -16,7 +17,7 @@ export const Router: React.FC = () => {
         <Routes>
           <Route path='/login' element={<MakeLogin />} />
           <Route path='/signup' element={<MakeSignup />} />
-          <Route path='/' element={<SurveyList />} />
+          <Route path='/' element={<PrivateRoute component={<SurveyList />} />} />
         </Routes>
       </BrowserRouter>
     </AccountContext.Provider>
