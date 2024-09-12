05ec6dfc4d1bb62919e9f6b36e6f0bc4bcab5053
Allysson Alas <allyssonmgs@gmail.com>
2024-09-12 12:39:32 -0300

    feat: ensure PrivateRoute renders component if token is provided

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AccountContext } from '@/presentation/contexts'
 import { MakeLogin, MakeSignup } from '@/main/factories/presentation/pages'
 import { SurveyList } from '@/presentation/pages'
-import { setCurrentAccountAdapter } from '@/main/adapters'
+import { setCurrentAccountAdapter, getCurrentAccountAdapter } from '@/main/adapters'
 
 import '@/presentation/styles/globals.scss'
 
 export const Router: React.FC = () => {
   return (
-    <AccountContext.Provider value={{ setCurrentAccount: setCurrentAccountAdapter }}>
+    <AccountContext.Provider
+      value={{ setCurrentAccount: setCurrentAccountAdapter, getCurrentAccount: getCurrentAccountAdapter }}
+    >
       <BrowserRouter>
         <Routes>
           <Route path='/login' element={<MakeLogin />} />
--- src/presentation/components/PrivateRoute/index.tsx
+++ src/presentation/components/PrivateRoute/index.tsx
@@ -1,5 +1,16 @@
+import React, { useContext } from 'react'
 import { Navigate } from 'react-router-dom'
 
-export const PrivateRoute: React.FC = () => {
-  return <Navigate to='/login' replace />
+import { AccountContext } from '@/presentation/contexts'
+
+type Props = {
+  component: React.ReactElement
+}
+
+export const PrivateRoute: React.FC<Props> = ({ component }) => {
+  const { getCurrentAccount } = useContext(AccountContext)
+
+  const token = getCurrentAccount()?.accessToken
+
+  return token ? component : <Navigate to='/login' replace />
 }
