f7af16e0a90f322317b8c00351d0645f64b6545e
Allysson Alas <allyssonmgs@gmail.com>
2024-09-12 12:39:45 -0300

    test: ensure PrivateRoute renders component if token is provided

--- tests/presentation/components/private-route.spec.tsx
+++ tests/presentation/components/private-route.spec.tsx
@@ -2,6 +2,7 @@ import { Router } from 'react-router-dom'
 import { render } from '@testing-library/react'
 import { createMemoryHistory, MemoryHistory } from 'history'
 
+import { AccountContext } from '@/presentation/contexts'
 import { PrivateRoute } from '@/presentation/components'
 
 describe('PrivateRoute Component', () => {
@@ -9,14 +10,33 @@ describe('PrivateRoute Component', () => {
 
   beforeEach(() => {
     history = createMemoryHistory({ initialEntries: ['/'] })
-    render(
-      <Router location={history.location} navigator={history}>
-        <PrivateRoute />
-      </Router>,
-    )
   })
 
   it('Should redirect to /login if token is empty', () => {
+    const getCurrentAccount = () => null
+
+    render(
+      <AccountContext.Provider value={{ getCurrentAccount }}>
+        <Router location={history.location} navigator={history}>
+          <PrivateRoute component={<div />} />
+        </Router>
+      </AccountContext.Provider>,
+    )
+
     expect(history.location.pathname).toBe('/login')
+  })
+
+  it('Should render current component if token is not empty', () => {
+    const getCurrentAccount = () => ({ accessToken: 'any_access_token', name: 'any_name' })
+
+    render(
+      <AccountContext.Provider value={{ getCurrentAccount }}>
+        <Router location={history.location} navigator={history}>
+          <PrivateRoute component={<div />} />
+        </Router>
+      </AccountContext.Provider>,
+    )
+
+    expect(history.location.pathname).toBe('/')
   })
 })
