e6aea87dc66864d81d15f6d54d099e6c57f2654a
Allysson Alas <allyssonmgs@gmail.com>
2024-09-13 18:26:34 -0300

    test: ensure SurveyList renders SurveyItems on success

--- tests/presentation/pages/survey-list.spec.tsx
+++ tests/presentation/pages/survey-list.spec.tsx
@@ -1,29 +1,42 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 
 import { SurveyList } from '@/presentation/pages'
+
+import { mockLoadSurveyListOutput } from '@/tests/domain/mocks'
 
 type SutTypes = {
   loadSurveyList: jest.Mock
 }
 
 const makeSut = (): SutTypes => {
-  const loadSurveyList = jest.fn()
+  const loadSurveyList = jest.fn().mockResolvedValue(mockLoadSurveyListOutput())
   render(<SurveyList loadSurveyList={loadSurveyList} />)
   return { loadSurveyList }
 }
 
 describe('SurveyList Page', () => {
-  it('Should present 4 empty items on start', () => {
+  it('Should present 4 empty items on start', async () => {
     makeSut()
 
     const surveyList = screen.getByTestId('survey-list')
 
-    expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+    expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+    await waitFor(() => surveyList)
   })
 
-  it('Should call LoadSurveyList', () => {
+  it('Should call LoadSurveyList', async () => {
     const { loadSurveyList } = makeSut()
 
     expect(loadSurveyList).toHaveBeenCalledTimes(1)
+    await waitFor(() => screen.getByRole('heading'))
+  })
+
+  it('Should render SurveyItems on success', async () => {
+    makeSut()
+
+    const surveyList = screen.getByTestId('survey-list')
+
+    await waitFor(() => surveyList)
+    expect(surveyList.querySelectorAll('li.surveyItemWrap')).toHaveLength(4)
   })
 })
