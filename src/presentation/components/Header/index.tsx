ddf054ab5516dd2ebb8ca8ebb8e0e14a948d435f
Allysson Alas <allyssonmgs@gmail.com>
2024-09-15 16:44:56 -0300

    feat: ensure logout calls setCurrentAccount with null

--- src/presentation/components/Header/index.tsx
+++ src/presentation/components/Header/index.tsx
@@ -1,19 +1,30 @@
-import { memo } from 'react'
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 
+import { AccountContext } from '@/presentation/contexts'
 import { Logo } from '@/presentation/components'
 
 import './styles.scss'
 
-export const Header: React.FC = memo(() => {
+export const Header: React.FC = () => {
+  const { setCurrentAccount } = useContext(AccountContext)
+  const navigate = useNavigate()
+  const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault()
+    setCurrentAccount(null)
+    navigate('/login', { replace: true })
+  }
   return (
     <header className={'headerWrap'}>
       <div className={'headerContent'}>
         <Logo />
         <div className={'logoutWrap'}>
           <span>Rodrigo</span>
-          <a href='#'>Sair</a>
+          <a data-testid='logout' href='#' onClick={logout}>
+            Sair
+          </a>
         </div>
       </div>
     </header>
   )
-})
+}
