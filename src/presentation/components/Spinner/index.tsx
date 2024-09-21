b55236922db7a7928f456654e9968e742e2f38d2
Allysson Alas <allyssonmgs@gmail.com>
2024-09-21 18:57:45 -0300

    fix: remove Dom warning

--- src/presentation/components/Spinner/index.tsx
+++ src/presentation/components/Spinner/index.tsx
@@ -4,8 +4,8 @@ type Props = React.HTMLAttributes<HTMLElement> & {
   isNegative?: boolean
 }
 
-export const Spinner: React.FC<Props> = (props: Props) => {
-  const negativeClass = props.isNegative ? 'negative' : ''
+export const Spinner: React.FC<Props> = ({ isNegative, ...props }: Props) => {
+  const negativeClass = isNegative ? 'negative' : ''
 
   return (
     <div {...props} className={['spinner', negativeClass, props.className].join(' ')} data-testid={'spinner'}>
