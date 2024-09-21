942d1f3107b56a15e7cf3f85dc9f7b650a404fb1
Allysson Alas <allyssonmgs@gmail.com>
2024-09-21 17:11:20 -0300

    feat: ensure LoadSurveyResult returns correct SurveyModel on 200

--- src/domain/usecases/load-survey-result.ts
+++ src/domain/usecases/load-survey-result.ts
@@ -1,15 +1,18 @@
+import { SurveyModel } from '@/domain/models/'
+import { SurveyApiModel } from '@/domain/models/externals'
 import { HttpGetClient, HttpStatusCode } from '@/domain/contracts/gateways'
 import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 
-export type LoadSurveyResult = () => Promise<void>
-type Setup = (url: string, httpGetClient: HttpGetClient) => LoadSurveyResult
+type Output = SurveyModel
+export type LoadSurveyResult = () => Promise<Output>
+type Setup = (url: string, httpGetClient: HttpGetClient<SurveyApiModel>) => LoadSurveyResult
 
 export const setupLoadSurveyResult: Setup = (url, httpGetClient) => {
   return async () => {
-    const { statusCode } = await httpGetClient.get({ url })
+    const { statusCode, body } = await httpGetClient.get({ url })
     switch (statusCode) {
       case HttpStatusCode.ok:
-        break
+        return { ...body, date: new Date(body.date) }
       case HttpStatusCode.forbidden:
         throw new AccessDeniedError()
       default:
