241740aae5c536763fab5c6d968cb274a0d088de
Allysson Alas <allyssonmgs@gmail.com>
2024-09-21 16:18:59 -0300

    feat: ensure LoadSurveyResult throws UnexpectedError on 404

--- src/domain/usecases/load-survey-result.ts
+++ src/domain/usecases/load-survey-result.ts
@@ -1,5 +1,5 @@
 import { HttpGetClient, HttpStatusCode } from '@/domain/contracts/gateways'
-import { AccessDeniedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 
 export type LoadSurveyResult = () => Promise<void>
 type Setup = (url: string, httpGetClient: HttpGetClient) => LoadSurveyResult
@@ -8,8 +8,12 @@ export const setupLoadSurveyResult: Setup = (url, httpGetClient) => {
   return async () => {
     const { statusCode } = await httpGetClient.get({ url })
     switch (statusCode) {
+      case HttpStatusCode.ok:
+        break
       case HttpStatusCode.forbidden:
         throw new AccessDeniedError()
+      default:
+        throw new UnexpectedError()
     }
   }
 }
