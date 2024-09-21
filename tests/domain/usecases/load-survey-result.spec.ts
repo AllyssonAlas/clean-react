bf332d10ec8c37c8b08085a8e288e5cb94036161
Allysson Alas <allyssonmgs@gmail.com>
2024-09-21 16:16:58 -0300

    test: ensure LoadSurveyResult throws AccessDenied on 403

--- tests/domain/usecases/load-survey-result.spec.ts
+++ tests/domain/usecases/load-survey-result.spec.ts
@@ -1,7 +1,8 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
 import { setupLoadSurveyResult, LoadSurveyResult } from '@/domain/usecases'
-import { HttpGetClient } from '@/domain/contracts/gateways'
+import { HttpGetClient, HttpStatusCode } from '@/domain/contracts/gateways'
+import { AccessDeniedError } from '@/domain/errors'
 
 describe('LoadSurveyResult', () => {
   let url: string
@@ -11,6 +12,7 @@ describe('LoadSurveyResult', () => {
   beforeAll(() => {
     url = 'any_url'
     httpGetClient = mock()
+    httpGetClient.get.mockResolvedValue({ statusCode: 200 })
   })
 
   beforeEach(() => {
@@ -22,5 +24,15 @@ describe('LoadSurveyResult', () => {
 
     expect(httpGetClient.get).toHaveBeenCalledWith({ url })
     expect(httpGetClient.get).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should throw AccessDenied if HttpPostClient returns 403', async () => {
+    httpGetClient.get.mockResolvedValueOnce({
+      statusCode: HttpStatusCode.forbidden,
+    })
+
+    const promise = sut()
+
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })
