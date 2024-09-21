1159d4549f12aa2dc3361e435b5eaa3dd8543596
Allysson Alas <allyssonmgs@gmail.com>
2024-09-21 19:21:25 -0300

    feat: add skeleton with shimmer on SurveyResult

--- src/presentation/pages/SurveyResult/index.tsx
+++ src/presentation/pages/SurveyResult/index.tsx
@@ -9,31 +9,35 @@ export const SurveyResult: React.FC = () => {
     <div className={'surveyResultWrap'}>
       <Header />
       <div className={'contentWrap'}>
-        <hgroup>
-          <Calendar date={new Date()} className={'calendarWrap'} />
-          <h2>
-            Qual é seu framework web favorito? Qual é seu framework web favorito? Qual é seu framework web favorito?
-          </h2>
-        </hgroup>
-        <FlipMove className={'answersList'}>
-          <li>
-            <img src='http://fordevs.herokuapp.com/static/img/logo-react.png' />
-            <span className={'answer'}>ReactJS</span>
-            <span className={'percent'}>50%</span>
-          </li>
-          <li className={'active'}>
-            <img src='http://fordevs.herokuapp.com/static/img/logo-react.png' />
-            <span className={'answer'}>ReactJS</span>
-            <span className={'percent'}>50%</span>
-          </li>
-          <li>
-            <img src='http://fordevs.herokuapp.com/static/img/logo-react.png' />
-            <span className={'answer'}>ReactJS</span>
-            <span className={'percent'}>50%</span>
-          </li>
-        </FlipMove>
-        <button>Voltar</button>
-        {true && <Loading />}
+        {false && (
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={'calendarWrap'} />
+              <h2>
+                Qual é seu framework web favorito? Qual é seu framework web favorito? Qual é seu framework web favorito?
+              </h2>
+            </hgroup>
+            <FlipMove className={'answersList'}>
+              <li>
+                <img src='http://fordevs.herokuapp.com/static/img/logo-react.png' />
+                <span className={'answer'}>ReactJS</span>
+                <span className={'percent'}>50%</span>
+              </li>
+              <li className={'active'}>
+                <img src='http://fordevs.herokuapp.com/static/img/logo-react.png' />
+                <span className={'answer'}>ReactJS</span>
+                <span className={'percent'}>50%</span>
+              </li>
+              <li>
+                <img src='http://fordevs.herokuapp.com/static/img/logo-react.png' />
+                <span className={'answer'}>ReactJS</span>
+                <span className={'percent'}>50%</span>
+              </li>
+            </FlipMove>
+            <button>Voltar</button>
+          </>
+        )}
+        {false && <Loading />}
       </div>
       <Footer />
     </div>
