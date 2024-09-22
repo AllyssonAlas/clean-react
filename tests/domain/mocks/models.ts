df9365d606312fe661761a80aa660f8f86d3d860
Allysson Alas <allyssonmgs@gmail.com>
2024-09-22 15:29:41 -0300

    test: ensure SurveyResult presents correct survey on success

--- tests/domain/mocks/models.ts
+++ tests/domain/mocks/models.ts
@@ -5,7 +5,10 @@ export const mockAccountModel = (): AccountModel => ({ accessToken: 'any_access_
 export const mockSurveyModel = (): SurveyModel => ({
   id: (Math.random() + 1).toString(36).substring(7),
   question: 'any_question',
-  answers: [{ answer: 'any_answer_1' }, { answer: 'any_answer_2' }],
+  answers: [
+    { answer: 'any_answer_1', image: 'any_image', count: 45, percent: 45, isCurrentAccountAnswer: true },
+    { answer: 'any_answer_2', count: 55, percent: 55, isCurrentAccountAnswer: false },
+  ],
   date: new Date('09/07/2022'),
   didAnswer: true,
 })
--- tests/presentation/pages/survey-result.spec.tsx
+++ tests/presentation/pages/survey-result.spec.tsx
@@ -5,15 +5,14 @@ import { createMemoryHistory } from 'history'
 import { AccountContext } from '@/presentation/contexts'
 import { SurveyResult } from '@/presentation/pages'
 
-import { mockAccountModel } from '@/tests/domain/mocks'
+import { mockAccountModel, mockSurveyModel } from '@/tests/domain/mocks'
 
 type SutTypes = {
   loadSurveyResult: jest.Mock
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (loadSurveyResult = jest.fn().mockResolvedValue(mockSurveyModel())): SutTypes => {
   const history = createMemoryHistory({ initialEntries: ['/'] })
-  const loadSurveyResult = jest.fn()
   render(
     <AccountContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
       <Router location={history.location} navigator={history}>
@@ -44,5 +43,34 @@ describe('SurveyResult Page', () => {
     expect(loadSurveyResult).toHaveBeenCalledTimes(1)
     expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
     expect(screen.queryByTestId('error')).not.toBeInTheDocument()
+  })
+
+  it('Should present survey on success', async () => {
+    const survey = Object.assign({}, mockSurveyModel(), {
+      date: new Date('2020-01-10T00:00:00'),
+    })
+    const loadSurveyResult = jest.fn().mockResolvedValue(survey)
+
+    makeSut(loadSurveyResult)
+    await waitFor(() => screen.getByTestId('survey-result'))
+
+    expect(screen.getByTestId('day')).toHaveTextContent('10')
+    expect(screen.getByTestId('month')).toHaveTextContent('jan')
+    expect(screen.getByTestId('year')).toHaveTextContent('2020')
+    expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('answers').childElementCount).toBe(2)
+    const answersWrap = screen.queryAllByTestId('answer-wrap')
+    expect(answersWrap[0]).toHaveClass('active')
+    expect(answersWrap[1]).not.toHaveClass('active')
+    const images = screen.queryAllByTestId('image')
+    expect(images[0]).toHaveAttribute('src', survey.answers[0].image)
+    expect(images[0]).toHaveAttribute('alt', survey.answers[0].answer)
+    expect(images[1]).toBeFalsy()
+    const answers = screen.queryAllByTestId('answer')
+    expect(answers[0]).toHaveTextContent(survey.answers[0].answer)
+    expect(answers[1]).toHaveTextContent(survey.answers[1].answer)
+    const percents = screen.queryAllByTestId('percent')
+    expect(percents[0]).toHaveTextContent(`${survey.answers[0].percent}%`)
+    expect(percents[1]).toHaveTextContent(`${survey.answers[1].percent}%`)
   })
 })
