f813701d429a8f2d2f305b93d936a0720b226fcc
Allysson Alas <allyssonmgs@gmail.com>
2024-09-24 19:24:25 -0300

    feat: ensure SaveSurveyResult throws AccessDeniedError on 403

--- src/domain/usecases/save-survey-result.ts
+++ src/domain/usecases/save-survey-result.ts
@@ -1,4 +1,5 @@
-import { HttpClient } from '@/domain/contracts/gateways'
+import { HttpClient, HttpStatusCode } from '@/domain/contracts/gateways'
+import { AccessDeniedError } from '@/domain/errors'
 
 type Input = { answer: string }
 export type SaveSurveyResult = (input: Input) => Promise<void>
@@ -6,6 +7,12 @@ type Setup = (url: string, httpClient: HttpClient<Input>) => SaveSurveyResult
 
 export const setupSaveSurveyResult: Setup = (url, httpClient) => {
   return async (input) => {
-    await httpClient.request({ url, method: 'put', params: input })
+    const { statusCode } = await httpClient.request({ url, method: 'put', params: input })
+    switch (statusCode) {
+      case HttpStatusCode.ok:
+        return null
+      case HttpStatusCode.forbidden:
+        throw new AccessDeniedError()
+    }
   }
 }
